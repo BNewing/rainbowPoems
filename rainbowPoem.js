6410856b7a3e4bbc29787a3b00e7003b52cada42
BNewing <bnewing92@hotmail.co.uk>
2016-08-08 16:08:08 +0100

    Colours added, timer still needs fixing

--- rainbowPoem.js
+++ rainbowPoem.js
@@ -2,6 +2,21 @@ var jabberwocky = ["JABBERWOCKY","Lewis Carroll","(from Through the Looking-Glas
 
 var printedJabberwocky = jabberwocky.join("");
 
+var x = document.getElementsByTagName("P");
+
+var i = 0;
+
+var j = 0;
+
+var k = 0;
+
+var l = 0;
+
+var m = 0;
+
+var n = 0;
+
+var p = 0;
 
 function displayPoem(){
 	var poemLength = jabberwocky.length;
@@ -13,22 +28,105 @@ function displayPoem(){
 	}
 }
 
-function start() {
-	turnColour();
-	var timer = setInterval(turnColour, 2000);
+function startRed(){
+	turnRed();
+	var timer = setInterval(turnRed, 200);
 }
 
-var i = 0
-
-function turnColour(){
-		var x = document.getElementsByTagName("P");
+function turnRed(){
+	if(i<x.length){
 		x[i].style.color = "red";
 		i = i + 1;
-
 	}
+	else{
+		startOrange();
+	}
+}
 
+function startOrange(){
+	turnOrange();
+	var timer = setInterval(turnOrange, 500);
+}
 
+function turnOrange(){
+	if(j<x.length){
+		x[j].style.color = "orange";
+		j = j + 1;
+	}
+	else{
+		startYellow();
+	}
+}
 
+function startYellow(){
+	turnYellow();
+	var timer = setInterval(turnYellow, 1000);
+}
 
+function turnYellow(){
+	if(k<x.length){
+		x[k].style.color = "yellow";
+		k = k + 1;
+	}
+	else{
+		startGreen();
+	}
+}
 
+function startGreen(){
+	turnGreen();
+	var timer = setInterval(turnGreen, 200);
+}
+
+function turnGreen(){
+	if(l<x.length){
+		x[l].style.color = "green";
+		l = l + 1;
+	}
+	else{
+		startBlue();
+	}
+}
+
+function startBlue(){
+	turnBlue();
+	var timer = setInterval(turnBlue, 200);
+}
+
+function turnBlue(){
+	if(m<x.length){
+		x[m].style.color = "blue";
+		m = m + 1;
+	}
+	else{
+		startIndigo();
+	}
+}
+
+function startIndigo(){
+	turnIndigo();
+	var timer = setInterval(turnIndigo, 200);
+}
+
+function turnIndigo(){
+	if(n<x.length){
+		x[n].style.color = "indigo";
+		n = n + 1;
+	}
+	else{
+		startViolet();
+	}
+}
+
+function startViolet(){
+	turnViolet();
+	var timer = setInterval(turnViolet, 200);
+}
+
+function turnViolet(){
+	if(p<x.length){
+		x[p].style.color = "violet";
+		p = p + 1;
+	}
+}
 
